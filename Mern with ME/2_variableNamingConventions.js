f30dbf4cd8041a42ada31bcc1795cf4b83b2080d
saifnihad300 <hossainnihad300@gmail.com>
2023-03-21 23:48:45 +0600

    me

---
+++ Mern with ME/2_variableNamingConventions.js
@@ -0,0 +1,10 @@
+
+var name = "Peter"
+var studyClass = 10
+var id = "1802065"
+var session = "2015-16"
+var email = "hossainnihad16@cse.pstu.ac.bd"
+var haveNationality = true;
+var userAddress = "132 st. pertersberg"
+
+console.log(userAddress)
