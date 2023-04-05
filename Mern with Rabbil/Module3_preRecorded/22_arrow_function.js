0a2bddf4c44929144cf9f8b0e12eb3437bec75cf
saifnihad300 <hossainnihad300@gmail.com>
2023-04-05 12:38:16 +0600

    ok

---
+++ Mern with Rabbil/Module3_preRecorded/22_arrow_function.js
@@ -0,0 +1,34 @@
+/**
+ * Arrow Function
+ * 
+ * To write smaller function syntax
+ * Arrow functions make your code more readable and structured
+ * Arrow functions are anonymous functions
+ * can declared without the function keyword.
+ * arrow functions cannot be used as the constructors.
+ */
+
+//declare arrow function
+
+// var myArrow=()=>{
+//     console.log("My first Arrow function")
+// }
+// myArrow();
+
+// var myArrow=(x)=>{
+//     console.log(x)
+// }
+// myArrow("My first Arrow function");
+
+// var myArrow=(...x)=>{
+//     console.log(x)
+// }
+// myArrow(1, 2, 3, 4, 5, 6, 7);
+
+var myArrow=()=>{
+    return "Hello"
+}
+console.log(myArrow());
+
+
+
